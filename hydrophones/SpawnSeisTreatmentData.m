5eeb95ad599931dae858145d2f71986f62e25b08
Nils Olav Handegard <nils.olav.handegard@imr.no>
2021-06-03 00:04:51 +0200

    Fixed a bug related to filename on missing files and skipped the file if no data were found within a deplyment for the treatment.

--- hydrophones/SpawnSeisTreatmentData.m
+++ hydrophones/SpawnSeisTreatmentData.m
@@ -20,44 +20,48 @@ for j=1:length(Tmeta_i.Hydrophone)
         T0 = datenum(Tmeta_i.Start,'dd.mm.yyyy HH:MM:SS');
         T1 = datenum(Tmeta_i.End,'dd.mm.yyyy HH:MM:SS');
         ind = find((T > T0) & (T < T1)); % misses parts of the first file.
-        if ind(1)>1
-            ind = (min(ind)-1):1:max(ind); % Add the first file
-        else
-            ind = (min(ind)):1:max(ind); % But not if it already is the first file
-        end
-        % Get calibration factor
-        cal = calibrationfactor(Tmeta_i.Hydrophone(j).Dmeta_index,1);
-        
-        % Extract the data for the treatment
-        Dat.Pressure=zeros(1,400000000);
-        Dat.Time=zeros(1,400000000);
-        Dat.T0 = T0;
-        k1=1;
-        % Loop over data files for this treatment
-        for k=1:length(ind)
-            F = fullfile(filelist(ind(k)).folder,filelist(ind(k)).name);
-            try
-            ainf = audioinfo(F);
-            dt = 1/ainf.SampleRate;
-            dat_sub = audioread(F,'native');
-            tim_sub = ((1:length(dat_sub))-1)*dt;% seconds
-            % Time for this file in seconds relative to start of treatment (T0)
-            T0_sub =  tim_sub + (T(ind(k))-T0)*3600*24;
-            % Adding data
-            k2 = k1-1+length(dat_sub);
-            Dat.Pressure(k1:k2) = double(dat_sub')*cal;
-            Dat.Time(k1:k2) = T0_sub;
-            k1 = k2+1;
-            catch
-                warning(['Cannot read ',F,' -> skipping file'])
+        if ~isempty(ind) % Check if there are missing times for the deployment
+            if ind(1)>1
+                ind = (min(ind)-1):1:max(ind); % Add the first file
+            else
+                ind = (min(ind)):1:max(ind); % But not if it already is the first file
             end
+            % Get calibration factor
+            cal = calibrationfactor(Tmeta_i.Hydrophone(j).Dmeta_index,1);
+            
+            % Extract the data for the treatment
+            Dat.Pressure=zeros(1,400000000);
+            Dat.Time=zeros(1,400000000);
+            Dat.T0 = T0;
+            k1=1;
+            % Loop over data files for this treatment
+            for k=1:length(ind)
+                try
+                    F = fullfile(filelist(ind(k)).folder,filelist(ind(k)).name);
+                    ainf = audioinfo(F);
+                    dt = 1/ainf.SampleRate;
+                    dat_sub = audioread(F,'native');
+                    tim_sub = ((1:length(dat_sub))-1)*dt;% seconds
+                    % Time for this file in seconds relative to start of treatment (T0)
+                    T0_sub =  tim_sub + (T(ind(k))-T0)*3600*24;
+                    % Adding data
+                    k2 = k1-1+length(dat_sub);
+                    Dat.Pressure(k1:k2) = double(dat_sub')*cal;
+                    Dat.Time(k1:k2) = T0_sub;
+                    k1 = k2+1;
+                catch
+                    warning(['Cannot read ',F,' -> skipping file'])
+                end
+            end
+            % Trim data files
+            Dat.Pressure = Dat.Pressure(1:k2);
+            Dat.Time = Dat.Time(1:k2);
+            
+            save(tmpfil,'Dat','-v7.3')
+            clear Dat
+        else
+            warning(['No data for Block',num2str(Tmeta_i.BlockNo),' Treatment',num2str(Tmeta_i.TreatmentNo),' Hydrophone',num2str(j)])
         end
-        % Trim data files
-        Dat.Pressure = Dat.Pressure(1:k2);
-        Dat.Time = Dat.Time(1:k2);
-        
-        save(tmpfil,'Dat','-v7.3')
-        clear Dat
     end
 end
 end
